4a3a736abd43d88c89b510b33deccd3065796d98
thuggeelya <ilya-fdmdh@mail.ru>
2021-12-18 23:08:58 +0700

    kljhgfd

--- src/main/java/org/example/app/services/LoginService.java
+++ src/main/java/org/example/app/services/LoginService.java
@@ -23,13 +23,13 @@ public class LoginService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    private User isRegistered(User loginForm) {
-        AtomicReference<User> regUser = new AtomicReference<>(loginForm);
+    private User isRegistered(String email) {
+        AtomicReference<User> regUser = new AtomicReference<>(null);
 
         jdbcTemplate.query("SELECT * FROM users_table", (ResultSet rs, int rowNum) -> {
             User user = new User();
-            if (rs.getString("email").equals(loginForm.getEmail())) {
-                logger.info("user " + loginForm.getEmail() + " is registered");
+            if (rs.getString("email").equals(email)) {
+                logger.info("user " + email + " is registered");
                 user.setEmail(rs.getString("email"));
                 user.setPassword(rs.getString("password"));
                 regUser.set(user);
@@ -43,7 +43,10 @@ public class LoginService {
     public boolean authenticate(@NotNull User loginForm) {
         logger.info("try auth with user-form: " + loginForm);
 
-        User user = isRegistered(loginForm);
+        User user = isRegistered(loginForm.getEmail());
+        if (user == null) {
+            return false;
+        }
 
         return loginForm.getPassword().equals(user.getPassword());
     }
@@ -51,18 +54,16 @@ public class LoginService {
     public boolean register(@NotNull User loginForm) {
         logger.info("register with user-form: " + loginForm);
 
-        User user = isRegistered(loginForm);
-        if (loginForm.getEmail().equals(user.getEmail()) && loginForm.getPassword().equals(user.getPassword())) {
-            return true;
+        User user = isRegistered(loginForm.getEmail());
+        if (user == null) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("id", loginForm.hashCode());
+            params.put("email", loginForm.getEmail());
+            params.put("password", loginForm.getPassword());
+            String exp = "INSERT INTO users_table(id,email,password) VALUES (:id,:email,:password)";
+            jdbcTemplate.update(exp, params);
+            return false;
         }
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("id", loginForm.hashCode());
-        params.put("email", loginForm.getEmail());
-        params.put("password", loginForm.getPassword());
-        String exp = "INSERT INTO users_table(id,email,password) VALUES (:id,:email,:password)";
-        jdbcTemplate.update(exp, params);
-
-        return false;
+        return true;
     }
 }
